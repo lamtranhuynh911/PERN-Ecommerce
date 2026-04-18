a0b11fbcdbca0f0c518db15525a8806fcf9693aa
lam-tran <tranhuynhlam911@gmail.com>
2026-04-19 02:55:22 +0700

    fix: added all needed environment variables

--- terraform-azure/modules/aks/output.tf
+++ terraform-azure/modules/aks/output.tf
@@ -9,3 +9,7 @@ output "kube_config" {
   value       = azurerm_kubernetes_cluster.aks.kube_config[0]
   sensitive   = true
 }
+
+output "kubelet_identity_object_id" {
+  value = azurerm_kubernetes_cluster.aks.kubelet_identity[0].object_id
+}
